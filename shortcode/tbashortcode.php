c6a72f411dabda7bb8911ee287ccdf6c02c38670
Tori Takashi <tori.toutoshi@gmail.com>
2023-06-20 20:55:26 +0900

    phpcbfによる修正

--- shortcode/tbashortcode.php
+++ shortcode/tbashortcode.php
@@ -8,23 +8,24 @@
 /**
  * ショートコードを登録する.
  */
-function wptba_shortcode() {
-	wp_enqueue_script(
-		'wptba-page-TBAWidget.js',
-		plugins_url( '/frontend/dist/assets/TBAWidget.js', dirname( __FILE__ ) ),
-		array(
-			'jquery',
-			'wp-element',
-		),
-		wp_rand(),
-		true
-	);
-	wp_localize_script(
-		'wptba-page-TBAWidget.js',
-		'wptba',
-		array(
-			'_wpnonce' => wp_create_nonce( 'wp_rest' ),
-		)
-	);
-	return '<div id="wptba-root"></div>';
+function wptba_shortcode()
+{
+    wp_enqueue_script(
+        'wptba-page-TBAWidget.js',
+        plugins_url('/frontend/dist/assets/TBAWidget.js', dirname(__FILE__)),
+        array(
+            'jquery',
+            'wp-element',
+        ),
+        wp_rand(),
+        true
+    );
+    wp_localize_script(
+        'wptba-page-TBAWidget.js',
+        'wptba',
+        array(
+            '_wpnonce' => wp_create_nonce('wp_rest'),
+        )
+    );
+    return '<div id="wptba-root"></div>';
 }

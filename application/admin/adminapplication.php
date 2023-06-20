f5a5b61afcb9b6706e219ca8999b8c07e442fc96
Tori Takashi <tori.toutoshi@gmail.com>
2023-06-21 00:46:46 +0900

    利用サービスとポリシー更新のApplication側を書いた

--- application/admin/adminapplication.php
+++ application/admin/adminapplication.php
@@ -50,4 +50,15 @@ class AdminApplication
             'service_official_policies_uids' => $service_official_policies_uids
         );
     }
+
+    /** 
+     * 利用しているサービスとポリシーを更新する.
+     * 
+     * @param array $service_and_policies_uids サービスとポリシーのUnique IDの配列.
+    */
+    public function updateServiceAndPolicies($service_and_policies_uids)
+    {
+        $service_policies = new ServicePolicies($this->service_policies_repository);
+        $service_policies->updateAllServiceAndPolicies($service_and_policies_uids);
+    }
 }

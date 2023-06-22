8ac9e405ee02cab8b00c378960fa680c1f00321e
Tori Takashi <tori.toutoshi@gmail.com>
2023-06-23 02:37:47 +0900

    change icon size

--- frontend/src/components/ServicePolicy/ServicePolicyShowMore/ServicePolicyShowMore.tsx
+++ frontend/src/components/ServicePolicy/ServicePolicyShowMore/ServicePolicyShowMore.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Text, HStack } from '@chakra-ui/react'
+import { Box, Text, HStack } from '@chakra-ui/react'
 import { AiOutlinePlus, AiOutlineMinus } from 'react-icons/ai'
 
 export interface ServicePolicyShowMoreProps {
@@ -9,25 +9,39 @@ export interface ServicePolicyShowMoreProps {
 
 const ServicePolicyShowMore: React.FC<ServicePolicyShowMoreProps> = (props: ServicePolicyShowMoreProps) => {
   return (
-        <HStack
-            spacing={0}
-            onClick={props.toggle}
-            _hover={
-                {
-                  cursor: 'pointer'
-                }
-            }
-        >
-            <Text
-                color={'#1E50B5'}
-            >
-                外部送信情報はこちら
-            </Text>
-            {props.opened
-              ? <AiOutlineMinus color={'#1E50B5'} />
-              : <AiOutlinePlus color={'#1E50B5'} />
-            }
-        </HStack>
+    <HStack
+      spacing={0}
+      onClick={props.toggle}
+      _hover={
+        {
+          cursor: 'pointer'
+        }
+      }
+    >
+      <Text
+        color={'#1E50B5'}
+      >
+        外部送信情報はこちら
+      </Text>
+      {props.opened
+        ? (
+          <Box
+            w={'1rem'}
+            h={'1rem'}
+          >
+            <AiOutlineMinus color={'#1E50B5'} />
+          </Box>
+          )
+        : (
+          <Box
+            w={'1rem'}
+            h={'1rem'}
+          >
+            <AiOutlinePlus color={'#1E50B5'} />
+          </Box>
+          )
+      }
+    </HStack>
   )
 }
 

585a53125f11c1d0aad639d3b1fa5858a19e1b3b
Tori Takashi <tori.toutoshi@gmail.com>
2023-06-23 01:04:59 +0900

    add chakra ui to storybook

--- frontend/.storybook/main.cjs
+++ frontend/.storybook/main.cjs
@@ -1,12 +1,13 @@
 module.exports = {
   "stories": ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
-  "addons": ["@storybook/addon-links", "@storybook/addon-essentials", "@storybook/addon-interactions", "@storybook/addon-mdx-gfm"],
+  "addons": ["@storybook/addon-links", "@storybook/addon-essentials", "@storybook/addon-interactions", "@storybook/addon-mdx-gfm", "@chakra-ui/storybook-addon"],
   "framework": {
     name: "@storybook/react-vite",
     options: {}
   },
   "features": {
-    "storyStoreV7": true
+    "storyStoreV7": true,
+    "emotionAlias": false
   },
   docs: {
     autodocs: true

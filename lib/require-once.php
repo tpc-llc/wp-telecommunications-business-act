b9be720de2db1af85af30e8415af99ad83d683fd
Tori Takashi <tori.toutoshi@gmail.com>
2023-06-28 23:03:54 +0900

    change load style

--- lib/require-once.php
+++ lib/require-once.php
@@ -7,18 +7,16 @@
 
 ?>
 <?php
-
 // Require necessary client files.
-require_once __DIR__ . '/client/lib/Api/DefaultApi.php';
-require_once __DIR__ . '/client/lib/ApiException.php';
-require_once __DIR__ . '/client/lib/Model/ModelInterface.php';
-require_once __DIR__ . '/client/lib/Configuration.php';
-require_once __DIR__ . '/client/lib/HeaderSelector.php';
-require_once __DIR__ . '/client/lib/ObjectSerializer.php';
+class_exists("DefaultApi") ? null : require_once __DIR__ . '/client/lib/Api/DefaultApi.php';
+class_exists("ApiException") ? null : require_once __DIR__ . '/client/lib/ApiException.php';
+class_exists("Configuration") ? null : require_once __DIR__ . '/client/lib/Configuration.php';
+class_exists("HeaderSelector") ? null : require_once __DIR__ . '/client/lib/HeaderSelector.php';
+class_exists("ObjectSerializer") ? null : require_once __DIR__ . '/client/lib/ObjectSerializer.php';
 
 // Require all models.
+class_exists("ModelInterface") ? null : require_once __DIR__ . '/client/lib/Model/ModelInterface.php';
 require_once __DIR__ . '/client/lib/Model/DeleteUsersRequest.php';
-require_once __DIR__ . '/client/lib/Model/ModelInterface.php';
 require_once __DIR__ . '/client/lib/Model/PatchUsersRequest.php';
 require_once __DIR__ . '/client/lib/Model/PostUsersRequest.php';
 require_once __DIR__ . '/client/lib/Model/PostUsersResponse.php';

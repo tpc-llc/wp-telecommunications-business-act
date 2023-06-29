e074f9910eda8bf0045095ae062cc9e2eba5b8d7
Tori Takashi <tori.toutoshi@gmail.com>
2023-06-29 14:41:11 +0900

    registerの登録忘れを修正

--- presentation/endpoint/admin/require-once.php
+++ presentation/endpoint/admin/require-once.php
@@ -10,4 +10,5 @@
 
 require_once __DIR__ . '/pageinfo/require-once.php';
 require_once __DIR__ . '/emailoptin/require-once.php';
+require_once __DIR__ . '/register/require-once.php';
 require_once __DIR__ . '/servicepolicies/require-once.php';

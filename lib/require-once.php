266cc87837df8c2139d9ad7d78b18d10423ef954
Tori Takashi <tori.toutoshi@gmail.com>
2023-06-29 17:50:00 +0900

    fix by lint

--- lib/require-once.php
+++ lib/require-once.php
@@ -10,12 +10,22 @@
 // Require necessary client files.
 
 require_once __DIR__ . '/client/lib/Api/UsersApi.php';
-class_exists("OpenAPI\Client\ApiException") ? null : require_once __DIR__ . '/client/lib/ApiException.php';
-class_exists("OpenAPI\Client\Configuration") ? null : require_once __DIR__ . '/client/lib/Configuration.php';
-class_exists("OpenAPI\Client\HeaderSelector") ? null : require_once __DIR__ . '/client/lib/HeaderSelector.php';
-class_exists("OpenAPI\Client\ObjectSerializer") ? null : require_once __DIR__ . '/client/lib/ObjectSerializer.php';
+class_exists(
+    "OpenAPI\Client\ApiException"
+) ? null : require_once __DIR__ . '/client/lib/ApiException.php';
+class_exists(
+    "OpenAPI\Client\Configuration"
+) ? null : require_once __DIR__ . '/client/lib/Configuration.php';
+class_exists(
+    "OpenAPI\Client\HeaderSelector"
+) ? null : require_once __DIR__ . '/client/lib/HeaderSelector.php';
+class_exists(
+    "OpenAPI\Client\ObjectSerializer"
+) ? null : require_once __DIR__ . '/client/lib/ObjectSerializer.php';
 
-interface_exists("OpenAPI\Client\Model\ModelInterface") ? null : require_once __DIR__ . '/client/lib/Model/ModelInterface.php';
+interface_exists(
+    "OpenAPI\Client\Model\ModelInterface"
+) ? null : require_once __DIR__ . '/client/lib/Model/ModelInterface.php';
 
 // Require all models.
 require_once __DIR__ . '/client/lib/Model/DeleteUsersRequest.php';
